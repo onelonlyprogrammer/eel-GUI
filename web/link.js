c7d5479f3f279ca0a3ef3fa110f8b6210d471d0c
onelonlyprogrammer <40187173+onelonlyprogrammer@users.noreply.github.com>
2019-07-23 17:26:47 -0400

    GUI

--- web/link.js
+++ web/link.js
@@ -1,42 +1,31 @@
-function test(){
+function test() {
     date = document.getElementById("Date_query").value;
     team = document.getElementById("Team_query").value;
     info_box = document.getElementById("info_box");
-    console.log(team.toUpperCase)
 
+    //Splits date input and tests to see it's in proper format
     var split_date = date.split("-");
-        if (parseInt(split_date[0]) > 1000){
-            console.log("pass 0")
+    if (split_date.length == 3
+        && parseInt(split_date[0]) > 1000
+        && parseInt(split_date[1]) <= 12 && parseInt(split_date[1]) > 0
+        && parseInt(split_date[2]) <= 32 && parseInt(split_date[2]) > 0) {
 
-            if (parseInt(split_date[1]) <= 12 && parseInt(split_date[1]) > 0){
-                console.log("pass 1")
-
-                if (parseInt(split_date[2]) <= 32 && parseInt(split_date[1]) > 0){
-
-                    if(team.toUpperCase == "A" || team.toUpperCase == "B" || team == null){    
-                        info_box.innerHTML = "Information entered successfully";
-                        console.log("pass 2")
-                        console.log(date)
-                    }
-                }
-                else{
-                    info_box.innerHTML = "Error: Invalid day";
-                    console.log("error: invalid day")
-                }
-            }
-            else{
-                info_box.innerHTML = "Error: Invalid month";
-                console.log("error: invalid month")
-            }
+        //Tests team input for proper value
+        if (team == "A" || team == "a" || team == "B" || team == "b" || team == null) {
+            //If both date and team values pass the tests send to python
+            eel.main(date, team)(function (ret) {
+            /*for testing*/ //eel.tester(date, team)(function (ret) {
+                console.log(ret);
+            });
+            info_box.innerHTML = "Information entered successfully";
         }
-        else{
-            info_box.innerHTML = "Error: Invalid year";
-            console.log("error: invalid year")
+        //Gives error if split_date fails any tests
+        else {
+            info_box.innerHTML = "Invalid team. Team input field must be A B or blank";
         }
-
-    console.log(split_date);
-
-    eel.tester()(function(ret){
-        console.log(ret);
-    });
+    }
+    //Gives error if team has incorrect value
+    else {
+        info_box.innerHTML = "Invalid date. Date input must be in YYYY-MM-DD format. Example: 2017-5-15";
+    }
 }
