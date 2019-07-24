174d9d0d932050e5a60de7a90983abfca4243555
onelonlyprogrammer <40187173+onelonlyprogrammer@users.noreply.github.com>
2019-07-24 06:51:36 -0400

    GUI

--- web/link.js
+++ web/link.js
@@ -1,6 +1,6 @@
 function test() {
     date = document.getElementById("Date_query").value;
-    team = document.getElementById("Team_query").value;
+    crew = document.getElementById("Crew_query").value;
     info_box = document.getElementById("info_box");
 
     //Splits date input and tests to see it's in proper format
@@ -10,21 +10,21 @@ function test() {
         && parseInt(split_date[1]) <= 12 && parseInt(split_date[1]) > 0
         && parseInt(split_date[2]) <= 32 && parseInt(split_date[2]) > 0) {
 
-        //Tests team input for proper value
-        if (team == "A" || team == "a" || team == "B" || team == "b" || team == null) {
-            //If both date and team values pass the tests send to python
-            eel.main(date, team)(function (ret) {
-            /*for testing*/ //eel.tester(date, team)(function (ret) {
+        //Tests crew input for proper value
+        if (crew == "A" || crew == "a" || crew == "B" || crew == "b" || !crew) {
+            //If both date and crew values pass the tests send to python
+            //eel.main(date, crew)(function (ret) {
+            /*for testing*/ eel.tester(date, crew)(function (ret) {
                 console.log(ret);
             });
             info_box.innerHTML = "Information entered successfully";
         }
         //Gives error if split_date fails any tests
         else {
-            info_box.innerHTML = "Invalid team. Team input field must be A B or blank";
+            info_box.innerHTML = "Invalid crew. crew input field must be A B or blank";
         }
     }
-    //Gives error if team has incorrect value
+    //Gives error if crew has incorrect value
     else {
         info_box.innerHTML = "Invalid date. Date input must be in YYYY-MM-DD format. Example: 2017-5-15";
     }
