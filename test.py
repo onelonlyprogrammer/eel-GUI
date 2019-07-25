e53099c0535600105594c7aec1a3a374ab722bf9
Myles Rumph <40187173+onelonlyprogrammer@users.noreply.github.com>
2019-07-25 10:47:34 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import eel
 
-eel.init("web")
+eel.init("web") #telling eel where folder with html is
 
-@eel.expose
+@eel.expose #exposes function right below to javascript
 def main(date, team):
     print(date + team)
     return "got" + date + team
 
-eel.start('index.html', size=(500, 250))
+eel.start('index.html', size=(500, 250)) #lounches GUI
